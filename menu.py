db34cc462837cf1dc1f3d07a4329990a57123c67
namgwang <pieisland@naver.com>
2019-02-19 12:41:50 +0900

    init and menu update

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import nuke
 import nukescripts
+import checkenv
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("namgwang", icon="nam_logo.png")
@@ -10,3 +11,7 @@ m.addCommand("Slate/slate", "nuke.createNode('slate')")
 
 mb = menubar.addMenu("Nam")
 mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/lazypic/nuke/issues')")
+mb.addCommand("-", "", "")
+mb.addCommand("StartPerformanceTimers", "nuke.startPerformanceTimers()")
+mb.addCommand("StopPerformanceTimers", "nuke.stopPerformanceTimers()")
+mb.addCommand("Checkenv", "checkenv.main()")
