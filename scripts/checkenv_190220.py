17bec19bea1a74976cf9720755c26035d224270c
namgwang <pieisland@naver.com>
2019-02-20 10:12:58 +0900

    0220 checkenv

---
+++ scripts/checkenv_190220.py
@@ -0,0 +1,28 @@
+#coding:utf8
+import os
+from PySide2.QtWidgets import *
+
+class CheckEnv(QWidget):
+	layout = QVBoxLayout()
+	def __init__(self):
+		super(CheckEnv, self).__init__()
+		self.setLayout(self.layout)#QtWidgets의 method
+		self.setEnv()
+	
+	def setEnv(self):
+		envs=["USER", "OCIO", "NUKE_PATH", "NUKE_FONT_PATH"]
+		for e in envs:
+			self.layout.addWidget(QLabel("$%s : %s" % (e, os.environ)))
+
+def main():
+	global customApp
+	try:
+		customApp.close()
+	except:
+		pass
+	customApp=CheckEnv()
+	try:
+		customApp.show()
+	except:
+		pass
+	
