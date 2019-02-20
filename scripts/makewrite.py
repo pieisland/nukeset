e46d395bb0a5bad865f02d511a75a4b1bd9d2d0f
namgwang <pieisland@naver.com>
2019-02-20 12:13:02 +0900

    makewrite 수정

--- scripts/makewrite.py
+++ scripts/makewrite.py
@@ -2,6 +2,9 @@
 import nuke
 
 def main():
+	if len(nuke.selectedNodes()) != 1:
+		nuke.message("노드를 하나만 선택해주세요.")
+		return
 	tail = nuke.selectedNode()
 	w = nuke.nodes.Write()
 	w.setInput(0, tail)
